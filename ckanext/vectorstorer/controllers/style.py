e3022de974e86fe0742d5d82da412fdd3595bd09
derwas <derguech@gmail.com>
2018-09-24 15:03:28 +0000

    get the actual layer name, rather than calculating it

--- ckanext/vectorstorer/controllers/style.py
+++ ckanext/vectorstorer/controllers/style.py
@@ -88,7 +88,7 @@ class StyleController(BaseController):
             c.pkg = context['package']
             c.pkg_dict = c.package
             if c.resource.has_key('vectorstorer_resource') and c.resource['format'].lower()=='wms':
-                   c.layer_id=c.resource['parent_resource_id']
+                   c.layer_id=c.resource['wms_layer']
             else:
                 raise NotVectorStorerWMS
         except NotVectorStorerWMS:
