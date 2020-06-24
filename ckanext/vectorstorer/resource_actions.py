2106f6c08895abb588eb5bb0fb3d74c30b8a1434
Eric Soroos <eric@derilinx.com>
2020-06-24 15:54:15 +0000

    Check the number of child resources when enqueuing jobs
    * Create shouldn't have any
    * Update should have 2
    * Log a warning if there are more than 2

--- ckanext/vectorstorer/resource_actions.py
+++ ckanext/vectorstorer/resource_actions.py
@@ -72,25 +72,43 @@ def get_context(additional=None):
     return json.dumps(context)
 
 def create_vector_storer_task(resource, extra_params = None):
-    resource_package_id = resource.as_dict()['package_id']
+    res_dict = resource.as_dict()
+    resource_package_id = res_dict['package_id']
     extra_items = {'package_id': resource_package_id}
     if extra_params:
         extra_items.update(extra_params)
     context = get_context(extra_items)
     geoserver_context = get_geoserver_context()
     data = json.dumps(resource_dictize(resource, {'model': model}))
-    log.debug('create vectorstore task')
+    log.debug('create vectorstore task, pkg: %s, resource: %s, format: %s',
+              resource_package_id, res_dict['id'], res_dict['format'])
+    try:
+        child_resources = get_child_resources(res_dict)
+        if len(child_resources):
+            log.error("create vectorstore task: child resources found, bailing.")
+            return
+    except Exception as msg:
+        log.error("create vectorstore task: exception checking for children", msg)
+
     jobs.enqueue(tasks.vectorstorer_upload, [geoserver_context, context, data])
 
 
 def update_vector_storer_task(resource):
-    resource_package_id = resource.as_dict()['package_id']
-    resource_list_to_delete = _get_child_resources(resource.as_dict())
-    context = get_context({'resource_list_to_delete': resource_list_to_delete,
+    res_dict = resource.as_dict()
+    resource_package_id = res_dict['package_id']
+    resource_list_to_update = get_child_resources(res_dict)
+    log.debug('update vectorstore task: pkg: %s, resource: %s, format: %s child_resources: %s',
+              resource_package_id, res_dict['id'], res_dict['format'], resource_list_to_update)
+    if len(resource_list_to_update) < 2:
+        log.debug('not updating, not enough resources to update')
+        return
+    elif len(resource_list_to_update) > 2:
+        log.error("Duplicated WMS/DBTable, too many resources, continuing")
+
+    context = get_context({'resource_list_to_update': resource_list_to_update,
                            'package_id': resource_package_id})
-    geoserver_context = get_geoserver_context()
+    geoserver_context =  get_geoserver_context()
     data = json.dumps(resource_dictize(resource, {'model': model}))
-    log.debug('update vectorstore task')
     jobs.enqueue(tasks.vectorstorer_update, [geoserver_context, context, data])
 
 
